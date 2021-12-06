7f4a61c476d139c6ca5848e79e534d205429277c
cureforoptimism <cure_for_optimism@protonmail.com>
2021-12-05 22:55:57 -0700

    Cleanup

--- src/main/java/com/cureforoptimism/mbot/repository/TraitsRepository.java
+++ src/main/java/com/cureforoptimism/mbot/repository/TraitsRepository.java
@@ -14,19 +14,8 @@ public interface TraitsRepository extends JpaRepository<Trait, Long> {
 
   long countByTypeIgnoreCaseAndValueIgnoreCase(String type, String value);
 
-  long countByValueIgnoreCase(String value);
-
   @Query(
       value = "SELECT DISTINCT value FROM trait WHERE type = ?1 ORDER BY value asc",
       nativeQuery = true)
   List<String> findDistinctByTypeIgnoreCaseOrderByValueAsc(String type);
-  //  @Query(value = "SELECT distinct t FROM trait t.type", nativeQuery = true)
-  //  Set<String> distinctTypes();
-
-  //  @Query("select distinct type")
-  //  Set<String> types();
-  //    @Query("SELECT DISTINCT type FROM trait")
-  //    Set<String> findDistinctTypes();
-  //    public Set<String> findDistinctTypeStrings();
-
 }
