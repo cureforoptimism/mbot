c3287e5cbac97e57faa05d8767797bc85034063b
cureforoptimism <cure_for_optimism@protonmail.com>
2022-01-13 20:49:09 -0700

    Add slash command for /magic

--- src/main/java/com/cureforoptimism/mbot/discord/command/MagicCommand.java
+++ src/main/java/com/cureforoptimism/mbot/discord/command/MagicCommand.java
@@ -37,85 +37,76 @@ public class MagicCommand implements MbotCommand {
 
   @Override
   public Mono<Message> handle(MessageCreateEvent event) {
-    CoinFullData coinData = coinGeckoService.getCoinFullData();
-
-    return event
-        .getMessage()
-        .getChannel()
-        .flatMap(
-            c ->
-                c.createMessage(
-                    EmbedCreateSpec.builder()
-                        .title("MAGIC - $" + discordBot.getCurrentPrice())
-                        .description(
-                            "MC Rank: #"
-                                + coinData.getMarketCapRank()
-                                + "\n"
-                                + "Market cap: $"
-                                + NumberFormat.getIntegerInstance()
-                                    .format(coinData.getMarketData().getMarketCap().get("usd"))
-                                + "\n"
-                                + "24 hour volume: $"
-                                + NumberFormat.getIntegerInstance()
-                                    .format(discordBot.getCurrentVolume24h())
-                                + "\n"
-                                + "In circulation: "
-                                + NumberFormat.getIntegerInstance()
-                                    .format(coinData.getMarketData().getCirculatingSupply())
-                                + " MAGIC\n"
-                                + "Total supply: "
-                                + NumberFormat.getIntegerInstance()
-                                    .format(coinData.getMarketData().getTotalSupply())
-                                + " MAGIC\n"
-                                + "Max supply: "
-                                + NumberFormat.getIntegerInstance()
-                                    .format(coinData.getMarketData().getMaxSupply())
-                                + " MAGIC")
-                        .addField(
-                            "Current Prices",
-                            "USD: `"
-                                + discordBot.getCurrentPrice()
-                                + "`\n"
-                                + "ETH: `"
-                                + String.format(
-                                    "`%.6f`", coinData.getMarketData().getCurrentPrice().get("eth"))
-                                + "`\n"
-                                + "BTC: `"
-                                + String.format(
-                                    "`%.8f`", coinData.getMarketData().getCurrentPrice().get("btc"))
-                                + "`\n",
-                            true)
-                        .addField(
-                            "Price Changes",
-                            "1h: `"
-                                + String.format("`%.2f%%`", discordBot.getCurrentChange1h())
-                                + "`\n"
-                                + "24h: `"
-                                + String.format("`%.2f%%`", discordBot.getCurrentChange())
-                                + "`\n"
-                                + "7d: `"
-                                + String.format(
-                                    "`%.2f%%`",
-                                    coinData.getMarketData().getPriceChangePercentage7d())
-                                + "`\n"
-                                + "1m: `"
-                                + String.format(
-                                    "`%.2f%%`",
-                                    coinData.getMarketData().getPriceChangePercentage30d())
-                                + "`\n",
-                            true)
-                        .thumbnail(
-                            "https://assets.coingecko.com/coins/images/18623/large/Magic.png?1635755672")
-                        .footer(
-                            Footer.of(
-                                "Powered by Defined.fi",
-                                "https://miro.medium.com/fit/c/262/262/1*GQz1T6gRmenQRhTS-aWiLA.png"))
-                        .timestamp(Instant.now())
-                        .build()));
+    return event.getMessage().getChannel().flatMap(c -> c.createMessage(getMagicEmbed()));
   }
 
   @Override
   public Mono<Void> handle(ChatInputInteractionEvent event) {
-    return null;
+    event.reply().withEmbeds(getMagicEmbed()).block();
+
+    return Mono.empty();
+  }
+
+  private EmbedCreateSpec getMagicEmbed() {
+    CoinFullData coinData = coinGeckoService.getCoinFullData();
+
+    return EmbedCreateSpec.builder()
+        .title("MAGIC - $" + discordBot.getCurrentPrice())
+        .description(
+            "MC Rank: #"
+                + coinData.getMarketCapRank()
+                + "\n"
+                + "Market cap: $"
+                + NumberFormat.getIntegerInstance()
+                    .format(coinData.getMarketData().getMarketCap().get("usd"))
+                + "\n"
+                + "24 hour volume: $"
+                + NumberFormat.getIntegerInstance().format(discordBot.getCurrentVolume24h())
+                + "\n"
+                + "In circulation: "
+                + NumberFormat.getIntegerInstance()
+                    .format(coinData.getMarketData().getCirculatingSupply())
+                + " MAGIC\n"
+                + "Total supply: "
+                + NumberFormat.getIntegerInstance()
+                    .format(coinData.getMarketData().getTotalSupply())
+                + " MAGIC\n"
+                + "Max supply: "
+                + NumberFormat.getIntegerInstance().format(coinData.getMarketData().getMaxSupply())
+                + " MAGIC")
+        .addField(
+            "Current Prices",
+            "USD: `"
+                + discordBot.getCurrentPrice()
+                + "`\n"
+                + "ETH: `"
+                + String.format("`%.6f`", coinData.getMarketData().getCurrentPrice().get("eth"))
+                + "`\n"
+                + "BTC: `"
+                + String.format("`%.8f`", coinData.getMarketData().getCurrentPrice().get("btc"))
+                + "`\n",
+            true)
+        .addField(
+            "Price Changes",
+            "1h: `"
+                + String.format("`%.2f%%`", discordBot.getCurrentChange1h())
+                + "`\n"
+                + "24h: `"
+                + String.format("`%.2f%%`", discordBot.getCurrentChange())
+                + "`\n"
+                + "7d: `"
+                + String.format("`%.2f%%`", coinData.getMarketData().getPriceChangePercentage7d())
+                + "`\n"
+                + "1m: `"
+                + String.format("`%.2f%%`", coinData.getMarketData().getPriceChangePercentage30d())
+                + "`\n",
+            true)
+        .thumbnail("https://assets.coingecko.com/coins/images/18623/large/Magic.png?1635755672")
+        .footer(
+            Footer.of(
+                "Powered by Defined.fi",
+                "https://miro.medium.com/fit/c/262/262/1*GQz1T6gRmenQRhTS-aWiLA.png"))
+        .timestamp(Instant.now())
+        .build();
   }
 }
